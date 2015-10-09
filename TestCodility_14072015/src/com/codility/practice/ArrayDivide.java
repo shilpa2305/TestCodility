67b224c6f8aef05d7449bf8843a61e5e2bd1a879
Silpa Patnaik <Shilpa.Patnaik5@gmail.com>
2015-10-09 16:19:40 +0530

    TestCodility_14072015;

---
+++ TestCodility_14072015/src/com/codility/practice/ArrayDivide.java
@@ -0,0 +1,23 @@
+package com.codility.practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayDivide {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int K=3,M=5,A[]={2,1,5,1,2,2,2};
+		int N=A.length;
+		
+		for(int i=0;i<N;i++)
+		{
+			
+		}
+		
+	}
+
+}
